84b9d941706fd4b9b2973a2909cf1290b4ce9092
Andrea <a.puenchera@gmail.com>
2021-04-18 11:16:41 +0200

    Build hw3

--- noober.js
+++ noober.js
@@ -13,5 +13,51 @@ document.addEventListener('DOMContentLoaded', async function() {
   // 🔥 YOUR CODE GOES HERE 🔥
   // Write the recipe (algorithm) in the comments. Then, write the code.
   
+  /*  
+  Parse data and create variables
+  Number passengers, purple request binary, First & last name, 
+  phone number, pickup location, dropoff location */
+
+  let numberPassengers = ride.numberOfPassengers
+  let purpleRequest = ride.purpleRequested
+  let firstName = ride.passengerDetails.first
+  let lastName = ride.passengerDetails.last
+  let phoneNumber = ride.passengerDetails.phoneNumber
+  let pickupAddress = ride.pickupLocation.address
+  let pickupCity = ride.pickupLocation.city
+  let pickupState = ride.pickupLocation.state
+  let pickupZip = ride.pickupLocation.zip
+  let dropoffAddress = ride.dropoffLocation.address
+  let dropoffCity = ride.dropoffLocation.city
+  let dropoffState = ride.dropoffLocation.state
+  let dropoffZip = ride.dropoffLocation.zip
+    
+  /*  
+  name variable nooberType
+  Create conditional statement for Purple request and XL need
+  (if # passengers > 3 then XL else X, if Purple then always Purple) */
+  
+  let nooberType
+
+  if (purpleRequest == true) 
+    nooberType = `Purple`
+  else if (numberPassengers > 3)
+    nooberType = `XL`
+  else 
+    nooberType = `X`
+
+  
+  // Print to console
+
+  console.log(`Noober ${nooberType} Passenger: ${firstName} ${lastName} - ${phoneNumber}. 
+  Pickup at ${pickupAddress}, ${pickupCity}, ${pickupState} ${pickupZip}. 
+  Drop-off at ${dropoffAddress}, ${dropoffCity}, ${dropoffState} ${dropoffZip}`)
+
+
+
+
+
+
+
   // 🔥 YOUR CODE ENDS HERE 🔥
 })
